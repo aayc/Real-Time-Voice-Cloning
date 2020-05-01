ffb5bf84560b9377f20f2230df596a8b5f6546e0
Aaron Chan <aaron.y.chan64@gmail.com>
2020-05-01 14:15:28 -0400

    Remove inf loop

--- demo_cli.py
+++ demo_cli.py
@@ -94,93 +94,86 @@ if __name__ == '__main__':
     print("\tTesting the synthesizer... (loading the model will output a lot of text)")
     mels = synthesizer.synthesize_spectrograms(texts, embeds)
     
-    # The vocoder synthesizes one waveform at a time, but it's more efficient for long ones. We 
-    # can concatenate the mel spectrograms to a single one.
-    mel = np.concatenate(mels, axis=1)
-    # The vocoder can take a callback function to display the generation. More on that later. For 
-    # now we'll simply hide it like this:
-    no_action = lambda *args: None
-    print("\tTesting the vocoder...")
-    # For the sake of making this test short, we'll pass a short target length. The target length 
-    # is the length of the wav segments that are processed in parallel. E.g. for audio sampled 
-    # at 16000 Hertz, a target length of 8000 means that the target audio will be cut in chunks of
-    # 0.5 seconds which will all be generated together. The parameters here are absurdly short, and 
-    # that has a detrimental effect on the quality of the audio. The default parameters are 
-    # recommended in general.
-    vocoder.infer_waveform(mel, target=200, overlap=50, progress_callback=no_action)
-    
-    print("All test passed! You can now synthesize speech.\n\n")
-    
-    
-    ## Interactive speech generation
-    print("This is a GUI-less example of interface to SV2TTS. The purpose of this script is to "
-          "show how you can interface this project easily with your own. See the source code for "
-          "an explanation of what is happening.\n")
-    
-    print("Interactive generation loop")
-    num_generated = 0
-    while True:
-        try:
-            print("Getting file ", args.input_audio)
-            in_fpath = args.input_audio
-            ## Computing the embedding
-            # First, we load the wav using the function that the speaker encoder provides. This is 
-            # important: there is preprocessing that must be applied.
+        # The vocoder synthesizes one waveform at a time, but it's more efficient for long ones. We 
+        # can concatenate the mel spectrograms to a single one.
+        mel = np.concatenate(mels, axis=1)
+        # The vocoder can take a callback function to display the generation. More on that later. For 
+        # now we'll simply hide it like this:
+        no_action = lambda *args: None
+        print("\tTesting the vocoder...")
+        # For the sake of making this test short, we'll pass a short target length. The target length 
+        # is the length of the wav segments that are processed in parallel. E.g. for audio sampled 
+        # at 16000 Hertz, a target length of 8000 means that the target audio will be cut in chunks of
+        # 0.5 seconds which will all be generated together. The parameters here are absurdly short, and 
+        # that has a detrimental effect on the quality of the audio. The default parameters are 
+        # recommended in general.
+        vocoder.infer_waveform(mel, target=200, overlap=50, progress_callback=no_action)
+        
+        print("All test passed! You can now synthesize speech.\n\n")
+        
+        
+        ## Interactive speech generation
+        print("This is a GUI-less example of interface to SV2TTS. The purpose of this script is to "
+            "show how you can interface this project easily with your own. See the source code for "
+            "an explanation of what is happening.\n")
+        
+        print("Interactive generation loop")
+        num_generated = 0
+        print("Getting file ", args.input_audio)
+        in_fpath = args.input_audio
+        ## Computing the embedding
+        # First, we load the wav using the function that the speaker encoder provides. This is 
+        # important: there is preprocessing that must be applied.
+        
+        # The following two methods are equivalent:
+        # - Directly load from the filepath:
+        preprocessed_wav = encoder.preprocess_wav(in_fpath)
+        # - If the wav is already loaded:
+        original_wav, sampling_rate = librosa.load(in_fpath)
+        preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+        print("Loaded file succesfully")
+        
+        # Then we derive the embedding. There are many functions and parameters that the 
+        # speaker encoder interfaces. These are mostly for in-depth research. You will typically
+        # only use this function (with its default parameters):
+        embed = encoder.embed_utterance(preprocessed_wav)
+        print("Created the embedding")
+        
+        
+        ## Generating the spectrogram
+        text = input("Write a sentence (+-20 words) to be synthesized:\n")
+        
+        # The synthesizer works in batch, so you need to put your data in a list or numpy array
+        texts = [text]
+        embeds = [embed]
+        # If you know what the attention layer alignments are, you can retrieve them here by
+        # passing return_alignments=True
+        specs = synthesizer.synthesize_spectrograms(texts, embeds)
+        spec = specs[0]
+        print("Created the mel spectrogram")
+        
+        
+        ## Generating the waveform
+        print("Synthesizing the waveform:")
+        # Synthesizing the waveform is fairly straightforward. Remember that the longer the
+        # spectrogram, the more time-efficient the vocoder.
+        generated_wav = vocoder.infer_waveform(spec)
+        
+        
+        ## Post-generation
+        # There's a bug with sounddevice that makes the audio cut one second earlier, so we
+        # pad it.
+        generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
+        
+        # Play the audio (non-blocking)
+        if not args.no_sound:
+            sd.stop()
+            sd.play(generated_wav, synthesizer.sample_rate)
             
-            # The following two methods are equivalent:
-            # - Directly load from the filepath:
-            preprocessed_wav = encoder.preprocess_wav(in_fpath)
-            # - If the wav is already loaded:
-            original_wav, sampling_rate = librosa.load(in_fpath)
-            preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
-            print("Loaded file succesfully")
-            
-            # Then we derive the embedding. There are many functions and parameters that the 
-            # speaker encoder interfaces. These are mostly for in-depth research. You will typically
-            # only use this function (with its default parameters):
-            embed = encoder.embed_utterance(preprocessed_wav)
-            print("Created the embedding")
-            
-            
-            ## Generating the spectrogram
-            text = input("Write a sentence (+-20 words) to be synthesized:\n")
-            
-            # The synthesizer works in batch, so you need to put your data in a list or numpy array
-            texts = [text]
-            embeds = [embed]
-            # If you know what the attention layer alignments are, you can retrieve them here by
-            # passing return_alignments=True
-            specs = synthesizer.synthesize_spectrograms(texts, embeds)
-            spec = specs[0]
-            print("Created the mel spectrogram")
-            
-            
-            ## Generating the waveform
-            print("Synthesizing the waveform:")
-            # Synthesizing the waveform is fairly straightforward. Remember that the longer the
-            # spectrogram, the more time-efficient the vocoder.
-            generated_wav = vocoder.infer_waveform(spec)
-            
-            
-            ## Post-generation
-            # There's a bug with sounddevice that makes the audio cut one second earlier, so we
-            # pad it.
-            generated_wav = np.pad(generated_wav, (0, synthesizer.sample_rate), mode="constant")
-            
-            # Play the audio (non-blocking)
-            if not args.no_sound:
-                sd.stop()
-                sd.play(generated_wav, synthesizer.sample_rate)
-                
-            # Save it on the disk
-            fpath = "demo_output_%02d.wav" % num_generated
-            print(generated_wav.dtype)
-            librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
-                                     synthesizer.sample_rate)
-            num_generated += 1
-            print("\nSaved output as %s\n\n" % fpath)
-            
-            
-        except Exception as e:
-            print("Caught exception: %s" % repr(e))
-            print("Restarting\n")
+        # Save it on the disk
+        fpath = "demo_output_%02d.wav" % num_generated
+        print(generated_wav.dtype)
+        librosa.output.write_wav(fpath, generated_wav.astype(np.float32), 
+                                synthesizer.sample_rate)
+        num_generated += 1
+        print("\nSaved output as %s\n\n" % fpath)
